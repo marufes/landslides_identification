14bd438f29cbd48ca1d2c43ebc7d216ce1de2fdd
Baban Gain <gainbaban@gmail.com>
2025-07-23 18:18:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -91,7 +91,7 @@ def main(args):
                                     transforms=get_transform(), txt_name="train.txt")
 
     val_dataset = VOCSegmentation(args.data_path,
-                                  transforms=get_transform(), txt_name="val.txt")
+                                  transforms=get_transform(), txt_name="test.txt")
 
     num_workers = 2                                                                                                   
 
@@ -112,52 +112,11 @@ def main(args):
 
     model = create_model(num_classes=num_classes, pretrain=False)                                                       # 导入模型
     model.to(device)
+    val_mean_loss, confmat = evaluate(model, val_loader, device=device, num_classes=num_classes)
 
-    params_to_optimize = [
-        {"params": [p for p in model.parameters() if p.requires_grad]}
-    ]
+    val_info = str(confmat)
+    print(val_info)
 
-    optimizer = torch.optim.Adam(params_to_optimize, lr=args.lr)
-
-    scaler = torch.cuda.amp.GradScaler() if args.amp else None
-
-    lr_scheduler = create_lr_scheduler(optimizer, len(train_loader), args.epochs, warmup=True)
-
-     # 定义早停策略
-    early_stopping = EarlyStopping(patience=10, verbose=True)
-    start_time = time.time()
-    for epoch in range(0, args.epochs):
-        train_mean_loss, lr = train_one_epoch(model, optimizer, train_loader, device, epoch,
-                                              lr_scheduler=lr_scheduler, print_freq=args.print_freq, scaler=scaler)
-
-        val_mean_loss, confmat = evaluate(model, val_loader, device=device, num_classes=num_classes)
-
-        val_info = str(confmat)
-        print(val_info)
-
-        # 写入日志文件
-        with open(results_file, "a") as f:
-            train_info = f"[epoch: {epoch}]\n" \
-                         f"train_loss: {train_mean_loss:.4f}\n" \
-                         f"val_loss: {val_mean_loss:.4f}\n" \
-                         f"lr: {lr:.6f}\n"
-            f.write(train_info + val_info + "\n\n")
-
-        loss_history.append_loss(train_mean_loss, val_mean_loss)
-
-        # 应用早停策略
-        early_stopping(val_mean_loss, model)
-        if early_stopping.early_stop:
-            print("Early stopping")
-            break
-
-        # 保存模型权重
-        torch.save(model.state_dict(),
-                   'save_weights/ep%03d-loss%.3f-val_loss%.3f.pth' % (epoch, train_mean_loss, val_mean_loss))
-
-    total_time = time.time() - start_time
-    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-    print("training time {}".format(total_time_str))
 
 def parse_args():
     import argparse
