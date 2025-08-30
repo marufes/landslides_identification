57d62cb67417f698c8315ba2967b17f4415bc43a
Baban Gain <gainbaban@gmail.com>
2025-08-30 16:29:39 +0530

    Update BFA_ResUnet.py

--- BFA_ResUnet.py
+++ BFA_ResUnet.py
@@ -1,98 +1,77 @@
 ########################################################################################################################
-# BFA_ResUnet
+# BFA_ResUnet (with edge detection at input stage)
 ########################################################################################################################
 
-
-
-#!/usr/bin/env python
-
-import getopt
-import numpy
-import PIL
-import PIL.Image
-import sys
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import OrderedDict
+from typing import Dict
 
-##########################################################
+from backbone_resnet import resnet50
+from Unet_decode import Up, OutConv
 
-torch.set_grad_enabled(False) # make sure to not compute gradients for computational performance
-
-torch.backends.cudnn.enabled = True # make sure to use cudnn for computational performance
-
-##########################################################
-
-args_strModel = 'bsds500' # only 'bsds500' for now
-args_strIn = './images/sample.png'
-args_strOut = './out.png'
-
-##########################################################
-
+# ============================================================
+# HED Network (already in your code)
+# ============================================================
 class Network(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
         self.netVggOne = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False)
+            nn.Conv2d(3, 64, 3, 1, 1), nn.ReLU(inplace=False),
+            nn.Conv2d(64, 64, 3, 1, 1), nn.ReLU(inplace=False)
         )
-
         self.netVggTwo = torch.nn.Sequential(
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-            torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False)
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, 3, 1, 1), nn.ReLU(inplace=False),
+            nn.Conv2d(128, 128, 3, 1, 1), nn.ReLU(inplace=False)
         )
-
         self.netVggThr = torch.nn.Sequential(
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-            torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False)
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 256, 3, 1, 1), nn.ReLU(inplace=False),
+            nn.Conv2d(256, 256, 3, 1, 1), nn.ReLU(inplace=False),
+            nn.Conv2d(256, 256, 3, 1, 1), nn.ReLU(inplace=False)
         )
-
         self.netVggFou = torch.nn.Sequential(
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-            torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False)
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(256, 512, 3, 1, 1), nn.ReLU(inplace=False),
+            nn.Conv2d(512, 512, 3, 1, 1), nn.ReLU(inplace=False),
+            nn.Conv2d(512, 512, 3, 1, 1), nn.ReLU(inplace=False)
         )
-
         self.netVggFiv = torch.nn.Sequential(
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-            torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            torch.nn.ReLU(inplace=False)
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(512, 512, 3, 1, 1), nn.ReLU(inplace=False),
+            nn.Conv2d(512, 512, 3, 1, 1), nn.ReLU(inplace=False),
+            nn.Conv2d(512, 512, 3, 1, 1), nn.ReLU(inplace=False)
         )
 
-        self.netScoreOne = torch.nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0)
-        self.netScoreTwo = torch.nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1, padding=0)
-        self.netScoreThr = torch.nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0)
-        self.netScoreFou = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
-        self.netScoreFiv = torch.nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0)
+        self.netScoreOne = nn.Conv2d(64, 1, 1)
+        self.netScoreTwo = nn.Conv2d(128, 1, 1)
+        self.netScoreThr = nn.Conv2d(256, 1, 1)
+        self.netScoreFou = nn.Conv2d(512, 1, 1)
+        self.netScoreFiv = nn.Conv2d(512, 1, 1)
 
-        self.netCombine = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=5, out_channels=1, kernel_size=1, stride=1, padding=0),
-            torch.nn.Sigmoid()
+        self.netCombine = nn.Sequential(
+            nn.Conv2d(5, 1, 1),
+            nn.Sigmoid()
         )
 
-        self.load_state_dict({ strKey.replace('module', 'net'): tenWeight for strKey, tenWeight in torch.hub.load_state_dict_from_url(url='http://content.sniklaus.com/github/pytorch-hed/network-' + args_strModel + '.pytorch', file_name='hed-' + args_strModel).items() })
-    # end
+        self.load_state_dict({
+            strKey.replace('module', 'net'): tenWeight
+            for strKey, tenWeight in torch.hub.load_state_dict_from_url(
+                url='http://content.sniklaus.com/github/pytorch-hed/network-bsds500.pytorch',
+                file_name='hed-bsds500'
+            ).items()
+        })
 
     def forward(self, tenInput):
         tenInput = tenInput * 255.0
-        tenInput = tenInput - torch.tensor(data=[104.00698793, 116.66876762, 122.67891434], dtype=tenInput.dtype, device=tenInput.device).view(1, 3, 1, 1)
+        tenInput = tenInput - torch.tensor(
+            [104.00698793, 116.66876762, 122.67891434],
+            dtype=tenInput.dtype,
+            device=tenInput.device
+        ).view(1, 3, 1, 1)
 
         tenVggOne = self.netVggOne(tenInput)
         tenVggTwo = self.netVggTwo(tenVggOne)
@@ -106,122 +85,46 @@ class Network(torch.nn.Module):
         tenScoreFou = self.netScoreFou(tenVggFou)
         tenScoreFiv = self.netScoreFiv(tenVggFiv)
 
-        tenScoreOne = torch.nn.functional.interpolate(input=tenScoreOne, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
-        tenScoreTwo = torch.nn.functional.interpolate(input=tenScoreTwo, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
-        tenScoreThr = torch.nn.functional.interpolate(input=tenScoreThr, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
-        tenScoreFou = torch.nn.functional.interpolate(input=tenScoreFou, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
-        tenScoreFiv = torch.nn.functional.interpolate(input=tenScoreFiv, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+        tenScoreOne = F.interpolate(tenScoreOne, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+        tenScoreTwo = F.interpolate(tenScoreTwo, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+        tenScoreThr = F.interpolate(tenScoreThr, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+        tenScoreFou = F.interpolate(tenScoreFou, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
+        tenScoreFiv = F.interpolate(tenScoreFiv, size=(tenInput.shape[2], tenInput.shape[3]), mode='bilinear', align_corners=False)
 
-        return self.netCombine(torch.cat([ tenScoreOne, tenScoreTwo, tenScoreThr, tenScoreFou, tenScoreFiv ], 1))
-    # end
-# end
+        return self.netCombine(torch.cat([tenScoreOne, tenScoreTwo, tenScoreThr, tenScoreFou, tenScoreFiv], 1))
 
+# Singleton pattern for edge detector
 netNetwork = None
-
-##########################################################
-
 def estimate(tenInput):
     global netNetwork
-
     if netNetwork is None:
-        netNetwork = Network().cuda().train(False)
-    # end
+        netNetwork = Network().cuda().eval()
+    return netNetwork(tenInput)[0: , :, :, :]
 
-    intWidth = tenInput.shape[2]
-    intHeight = tenInput.shape[1]
-
-    return netNetwork(tenInput.cuda().view(1, 3, intHeight, intWidth))[0, :, :, :].cpu()
-# end
-
-##########################################################
-
-
-from collections import OrderedDict
-from typing import Dict
-
-import torch
-import torch.nn as nn
-from torch import Tensor
-from backbone_resnet import resnet50
-from Unet_decode import Up, OutConv
-from torch.nn import functional as F
-
-import torch.nn.functional as F
-import cv2
-import numpy as np
-
-
+# ============================================================
+# BFA Module (feature + edge map fusion)
+# ============================================================
 class BFA(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(BFA, self).__init__()
         self.conv1x1 = nn.Conv2d(in_channels + 1, out_channels, kernel_size=1) 
         self.relu = nn.ReLU()
 
-    def forward(self, x):
-        # 将输入图像转换为灰度图像
-        gray_image = self.to_grayscale(x)
-        print("Shape of x:", x.shape)
+    def forward(self, feature_map, edge_image):
+        # Resize edge map to match feature spatial size
+        edge_resized = F.interpolate(edge_image, size=feature_map.shape[2:], mode='bilinear', align_corners=False)
 
-        # 使用Canny算子进行边缘检测
-        # Flip channel order safely
-        x_ = x[:, [2, 1, 0], :, :]  # BGR <-> RGB
-        
-        # Normalize if needed
-        tenInput = x_.float() * (1.0 / 255.0)
-        edge_image = estimate(tenInput)
-        # 将边缘图像与原图像进行通道级联
-        concatenated_image = torch.cat([x, edge_image], dim=1)
+        # Concatenate along channel dimension
+        concatenated = torch.cat([feature_map, edge_resized], dim=1)
 
-        # 通过1x1卷积动态调整特征权重
-        output = self.conv1x1(concatenated_image)
+        output = self.conv1x1(concatenated)
         output = self.relu(output)
-
         return output
 
-    def to_grayscale(self, x):
-        # 将输入图像转换为灰度图像
-        gray_image = 0.2989 * x[:, 0, :, :] + 0.5870 * x[:, 1, :, :] + 0.1140 * x[:, 2, :, :]
-        gray_image = gray_image.unsqueeze(1) 
-        return gray_image
-
-    def canny_edge_detection(self, gray_image):
-        # 将灰度图像转换为numpy数组
-        gray_image_np = gray_image.cpu().detach().numpy().astype(np.uint8)
-
-        # 使用Canny算子进行边缘检测
-        edge_image_np = np.zeros_like(gray_image_np)
-        for i in range(gray_image_np.shape[0]):
-            edge_image_np[i, 0] = cv2.Canny(gray_image_np[i, 0], 50, 150)
-
-        # 将边缘图像转换回PyTorch张量
-        edge_image = torch.from_numpy(edge_image_np).float().to(gray_image.device)
-        return edge_image
-
+# ============================================================
+# Helper: IntermediateLayerGetter
+# ============================================================
 class IntermediateLayerGetter(nn.ModuleDict):
-    """
-    Module wrapper that returns intermediate layers from a model
-
-    It has a strong assumption that the modules have been registered
-    into the model in the same order as they are used.
-    This means that one should **not** reuse the same nn.Module
-    twice in the forward if you want this to work.
-
-    Additionally, it is only able to query submodules that are directly
-    assigned to the model. So if `model` is passed, `model.feature1` can
-    be returned, but not `model.feature1.layer2`.
-
-    Args:
-        model (nn.Module): model on which we will extract the features
-        return_layers (Dict[name, new_name]): a dict containing the names
-            of the modules for which the activations will be returned as
-            the key of the dict, and the value of the dict is the name
-            of the returned activation (which the user can specify).
-    """
-    _version = 2
-    __annotations__ = {
-        "return_layers": Dict[str, str],
-    }
-
     def __init__(self, model: nn.Module, return_layers: Dict[str, str]) -> None:
         if not set(return_layers).issubset([name for name, _ in model.named_children()]):
             raise ValueError("return_layers are not present in model")
@@ -236,10 +139,10 @@ class IntermediateLayerGetter(nn.ModuleDict):
             if not return_layers:
                 break
 
-        super(IntermediateLayerGetter, self).__init__(layers)
+        super().__init__(layers)
         self.return_layers = orig_return_layers
 
-    def forward(self, x: Tensor) -> Dict[str, Tensor]:
+    def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
         out = OrderedDict()
         for name, module in self.items():
             x = module(x)
@@ -248,20 +151,28 @@ class IntermediateLayerGetter(nn.ModuleDict):
                 out[out_name] = x
         return out
 
-
+# ============================================================
+# Full BFA-ResUNet
+# ============================================================
 class BFA_resunet(nn.Module):
     def __init__(self, num_classes, pretrain_backbone: bool = False):
         super(BFA_resunet, self).__init__()
         backbone = resnet50()
 
         if pretrain_backbone:
-            backbone.load_state_dict(torch.load("resnet50.pth", map_location='cpu'),strict=False)
+            backbone.load_state_dict(torch.load("resnet50.pth", map_location='cpu'), strict=False)
 
         self.stage_out_channels = [64, 256, 512, 1024, 2048]
-        return_layers = {'relu': 'out0', 'layer1': 'out1', 'layer2': 'out2', 'layer3': 'out3', 'layer4': 'out4'}
+        return_layers = {
+            'relu': 'out0',
+            'layer1': 'out1',
+            'layer2': 'out2',
+            'layer3': 'out3',
+            'layer4': 'out4'
+        }
         self.backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
 
-        # 添加BFA模块
+        # BFA block for deepest feature
         self.bfa = BFA(in_channels=self.stage_out_channels[4], out_channels=self.stage_out_channels[4])
 
         c = self.stage_out_channels[4] + self.stage_out_channels[3]
@@ -277,17 +188,23 @@ class BFA_resunet(nn.Module):
 
     def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
         input_shape = x.shape[-2:]
-        result = OrderedDict()
+
+        # ---- Edge detection on raw input ----
+        edge_image = estimate(x)  # shape [B, 1, H, W]
+
+        # ---- Backbone ----
         backbone_out = self.backbone(x)
 
-        # BFA模块应用于于最深层的编码层特征图
-        backbone_out['out4'] = self.bfa(backbone_out['out4'])
+        # ---- Apply BFA at deepest layer ----
+        backbone_out['out4'] = self.bfa(backbone_out['out4'], edge_image)
 
+        # ---- Decoder ----
         x = self.up1(backbone_out['out4'], backbone_out['out3'])
         x = self.up2(x, backbone_out['out2'])
         x = self.up3(x, backbone_out['out1'])
         x = self.up4(x, backbone_out['out0'])
         x = self.conv(x)
+
         x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
-        result["out"] = x
-        return result
+
+        return {"out": x}
