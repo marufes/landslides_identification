cb984ae22a4fb879a74d3c9e478747d38e4cc0c9
MD MARUF HOSSAIN <96608781+marufes@users.noreply.github.com>
2025-11-01 00:47:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -67,7 +67,7 @@ def get_transform():
 def create_model(num_classes, pretrain=True):
     model =BFA_resunet(num_classes=num_classes, pretrain_backbone=True)
     if pretrain:
-        model_path = '/content/drive/MyDrive/Project/save_weights_16/ep002-loss0.212-val_loss0.126.pth'                                                                # 预训练权重
+        model_path = '/content/drive/MyDrive/Project/save_weights_FF08/ep005-loss0.084-val_loss0.106.pth'                                                                # 预训练权重
         print('Load weights {}.'.format(model_path))
         device          = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         model_dict      = model.state_dict()
